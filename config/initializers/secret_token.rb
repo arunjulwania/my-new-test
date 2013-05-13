67e21b2262e8b9ac0cb08bd43cf6bb727a56c997
cis <cis@CISM189.(none)>
2013-05-13 17:34:23 +0530

    first update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paypal::Application.config.secret_token = 'cb70445b09a7201edfb0fd16fc36cb7f24f823978c3f50cfca12b5d19436c8e30eaa49e39684bb7e72accf831b4be8a07d64aa0d913defe93da3b3a0f8325720'
